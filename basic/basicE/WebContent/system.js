e46404029c5442a59de4cd02d8e89c108649f5b6
togashi_takumi <ta-togashi@virtualex.co.jp>
2020-04-27 10:54:17 +0900

    add DeleteServlet

--- basic/basicE/WebContent/system.js
+++ basic/basicE/WebContent/system.js
@@ -58,6 +58,7 @@ var create = function(){
 	dataType:'json',
 	url : '/myFirstApp/DisplayServlet',
 	data : requestQuery,
+	async : false,
 	success : function(json) {
 	// サーバーとの通信に成功した時の処理
 	// 確認のために返却値を出力
---
+++ basic/basicE/src/app/DeleteServlet.java
@@ -0,0 +1,80 @@
+package app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Servlet implementation class DeleteServlet
+ */
+@WebServlet("/DeleteServlet")
+public class DeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String  delName= request.getParameter("delName");
+		try {
+
+			// JDBCドライバのロード
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		} catch (ClassNotFoundException e) {
+			// ドライバが設定されていない場合はエラーになります
+			throw new RuntimeException(String.format("JDBCドライバのロードに失敗しました。詳細:[%s]", e.getMessage()), e);
+		}
+
+		// データベースにアクセスするために、データベースのURLとユーザ名とパスワードを指定
+		String url = "jdbc:oracle:thin:@localhost:1521:XE";
+		String user = "basic";
+		String pass = "basic";
+
+		// 実行するSQL文
+		String sql = "DELETE FROM SYAIN_ZYOUHOU WHERE SHAIN_NAME = '"+delName+"'";
+		try (
+				// データベースへ接続します
+				Connection con = DriverManager.getConnection(url, user, pass);
+				// SQLの命令文を実行するための準備をおこないます
+				Statement stmt = con.createStatement();) {
+			// SQLの命令文を実行し、その件数をint型のresultCountに代入します
+			int resultCount = stmt.executeUpdate(sql);
+		} catch (Exception e) {
+			throw new RuntimeException(String.format("検索処理の実施中にエラーが発生しました。詳細:[%s]", e.getMessage()), e);
+		}
+		// アクセスした人に応答するためのJSONを用意する
+		PrintWriter pw = response.getWriter();
+		// JSONで出力する
+		pw.append(new ObjectMapper().writeValueAsString("ok"));
+
+	}
+
+}
