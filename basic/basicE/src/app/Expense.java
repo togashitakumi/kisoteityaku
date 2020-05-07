5af86f9e54876e06dcfb487c924ea3484272d9aa
togashi_takumi <ta-togashi@virtualex.co.jp>
2020-05-07 10:53:05 +0900

    Expenseクラスの作成

---
+++ basic/basicE/src/app/Expense.java
@@ -0,0 +1,85 @@
+package app;
+
+public class Expense {
+	private String appliId;
+	private String appliDay;
+	private String appliName;
+	private String title;
+	private String payName;
+	private String payment;
+	private String updateDay;
+	private String changerName;
+	private String status;
+	private String reason;
+	private String empId;
+	public Expense() {
+
+	}
+	public String getAppliId() {
+		return appliId;
+	}
+	public void setAppliId(String appliId) {
+		this.appliId = appliId;
+	}
+	public String getAppliDay() {
+		return appliDay;
+	}
+	public void setAppliDay(String appliDay) {
+		this.appliDay = appliDay;
+	}
+	public String getAppliName() {
+		return appliName;
+	}
+	public void setAppliName(String appliName) {
+		this.appliName = appliName;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPayName() {
+		return payName;
+	}
+	public void setPayName(String payName) {
+		this.payName = payName;
+	}
+	public String getPayment() {
+		return payment;
+	}
+	public void setPayment(String payment) {
+		this.payment = payment;
+	}
+	public String getUpdateDay() {
+		return updateDay;
+	}
+	public void setUpdateDay(String updateDay) {
+		this.updateDay = updateDay;
+	}
+	public String getChangerName() {
+		return changerName;
+	}
+	public void setChangerName(String changerName) {
+		this.changerName = changerName;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getReason() {
+		return reason;
+	}
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+
+}
