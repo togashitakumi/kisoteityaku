45bdbb19daffc2a27b62d3e5f5cc5f56bad9b838
togashi_takumi <ta-togashi@virtualex.co.jp>
2020-05-07 10:37:36 +0900

    search機能にlogin機能追加

--- basic/basicE/WebContent/system.js
+++ basic/basicE/WebContent/system.js
@@ -8,8 +8,8 @@ var display = function() {
 	// サーバーからデータを取得する
 	count = 0;
 	var requestQuery = {
-			q : 1
-		};
+		q : 1
+	};
 
 	$
 			.ajax({
@@ -32,41 +32,42 @@ var display = function() {
 										+ '</td>';
 								count++;
 							}
-							var editElemnt = '<button id="edit1" value="'+ userEmId+'">編集</button>';
+							var editElemnt = '<button id="edit1" value="'
+									+ userEmId + '">編集</button>';
 							$('#display').append(tableElemnt);
 							$('#editBoxMem').append(editElemnt);
-						}else{
+						} else {
 
-						// サーバーとの通信に成功した時の処理
-						// 確認のために返却値を出力
-						console.log('返却値', json);
-						// 取得したデータを画面に表示する
-						var tableElemnt = '';
-						for (var i = 0; i < json.length; i++) {
-							var display = json[i];
-							tableElemnt += '<tr> <td>' + display.syainId
-									+ '</td><td>' + display.syainName
-									+ '</td><td><button id="edit' + (i + 1)
-									+ '" value="' + display.syainId
-									+ '">編集</button></td>'
-									+ '<td><button id="delete' + (i + 1)
-									+ '" value="' + display.syainId
-									+ '">削除</button></td></tr>';
-							count++;
+							// サーバーとの通信に成功した時の処理
+							// 確認のために返却値を出力
+							console.log('返却値', json);
+							// 取得したデータを画面に表示する
+							var tableElemnt = '';
+							for (var i = 0; i < json.length; i++) {
+								var display = json[i];
+								tableElemnt += '<tr> <td>' + display.syainId
+										+ '</td><td>' + display.syainName
+										+ '</td><td><button id="edit' + (i + 1)
+										+ '" value="' + display.syainId
+										+ '">編集</button></td>'
+										+ '<td><button id="delete' + (i + 1)
+										+ '" value="' + display.syainId
+										+ '">削除</button></td></tr>';
+								count++;
 
-						}
-						var createElement = '<input type="text"placeholder="EMP????" id="newId"></input>'
-								+ '<input type="text"placeholder="名前" id="newName"></input>'
-								+ '<input type="text"placeholder="年齢" id="newAge"></input>'
-								+ '<p><label><input type="radio"name="sex" value="男">男</label>'
-								+ '<label><input type="radio"name="sex" value="女">女</label></p>'
-								+ '<input type="text"placeholder="P?????" id="newImgId"></input>'
-								+ '<input type="text"placeholder="〒〇〇〇-〇〇〇〇 住所" id="newAdress"></input>'
-								+ '<input type="text"placeholder="部署Id" id="newDpId"></input>'
-								+ '<button id="create">新規作成</button>';
-						// HTMLに挿入
-						$('#display').append(tableElemnt);
-						$('#createbox').append(createElement);
+							}
+							var createElement = '<input type="text"placeholder="EMP????" id="newId"></input>'
+									+ '<input type="text"placeholder="名前" id="newName"></input>'
+									+ '<input type="text"placeholder="年齢" id="newAge"></input>'
+									+ '<p><label><input type="radio"name="sex" value="男">男</label>'
+									+ '<label><input type="radio"name="sex" value="女">女</label></p>'
+									+ '<input type="text"placeholder="P?????" id="newImgId"></input>'
+									+ '<input type="text"placeholder="〒〇〇〇-〇〇〇〇 住所" id="newAdress"></input>'
+									+ '<input type="text"placeholder="部署Id" id="newDpId"></input>'
+									+ '<button id="create">新規作成</button>';
+							// HTMLに挿入
+							$('#display').append(tableElemnt);
+							$('#createbox').append(createElement);
 						}
 					}
 				},
@@ -291,6 +292,7 @@ var pull = function() {
 var search = function() {
 	// サーバーからデータを取得する
 	$('#display').empty();
+	$('#editBoxMem').empty();
 	var getDepartmentId = document.form.department;
 	var num = getDepartmentId.selectedIndex;
 	var departmentId = getDepartmentId.options[num].value;
@@ -315,19 +317,36 @@ var search = function() {
 				// 確認のために返却値を出力
 				console.log('返却値', json);
 				// 取得したデータを画面に表示する
-				var tableElemnt = '';
-				for (var i = 0; i < json.length; i++) {
-					var display = json[i];
-					tableElemnt += '<tr> <td>' + display.syainId + '</td><td>'
-							+ display.syainName + '</td><td><button id="edit'
-							+ (i + 1) + '" value="' + display.syainName
-							+ '">編集</button></td>' + '<td><button id="delete'
-							+ (i + 1) + '" value="' + display.syainName
-							+ '">削除</button></td></tr>';
-					count++;
-				}
+				if (userRole !== "マネージャー") {
+					var tableElemnt = '';
+					for (var i = 0; i < json.length; i++) {
+						var display = json[i];
+						tableElemnt += '<tr> <td>' + display.syainId
+								+ '</td><td>' + display.syainName + '</td>';
+						count++;
+					}
+					var editElemnt = '<button id="edit1" value="' + userEmId
+							+ '">編集</button>';
+					$('#display').append(tableElemnt);
+					$('#editBoxMem').append(editElemnt);
+				} else {
+					var tableElemnt = '';
+					for (var i = 0; i < json.length; i++) {
+						var display = json[i];
+						tableElemnt += '<tr> <td>' + display.syainId
+								+ '</td><td>' + display.syainName
+								+ '</td><td><button id="edit' + (i + 1)
+								+ '" value="' + display.syainName
+								+ '">編集</button></td>'
+								+ '<td><button id="delete' + (i + 1)
+								+ '" value="' + display.syainName
+								+ '">削除</button></td></tr>';
+						count++;
+					}
+
 				// HTMLに挿入
 				$('#display').append(tableElemnt);
+				}
 			}
 		},
 		error : function(XMLHttpRequest, textStatus, errorThrown) {
@@ -337,26 +356,26 @@ var search = function() {
 		}
 	});
 }
-var session = function(){
+var session = function() {
 	var requestQuery = {
-			q : 1
-		};
-		$.ajax({
-			type : 'POST',
-			dataType:'json',
-			url : '/myFirstApp/SessionServlet',
-			async : false,
-			data : requestQuery,
-			success : function(json) {
-					userEmId = json.userEmId;
-					userRole = json.userRole;
-			},
-			error:function(XMLHttpRequest, textStatus, errorThrown){
-				// サーバーとの通信に失敗した時の処理
-				alert('データの通信に失敗しました');
-				console.log(errorThrown)
-			}
-		});
+		q : 1
+	};
+	$.ajax({
+		type : 'POST',
+		dataType : 'json',
+		url : '/myFirstApp/SessionServlet',
+		async : false,
+		data : requestQuery,
+		success : function(json) {
+			userEmId = json.userEmId;
+			userRole = json.userRole;
+		},
+		error : function(XMLHttpRequest, textStatus, errorThrown) {
+			// サーバーとの通信に失敗した時の処理
+			alert('データの通信に失敗しました');
+			console.log(errorThrown)
+		}
+	});
 }
 $(document).ready(function() {
 	session();
